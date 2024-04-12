1894e8d6492d7e6b47d5894e0743cefd57433a92
Aridany <aridany54@gmail.com>
2024-04-13 00:27:20 +0100

    styles/app.css/header.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import "./Styles/app.css"
+import './styles/app.css'
+import './styles/header.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
