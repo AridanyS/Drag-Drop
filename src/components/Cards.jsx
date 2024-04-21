2a54b6563abe9a940c50254916f06a03068a9939
Aridany <aridany54@gmail.com>
2024-04-21 19:29:08 +0200

    AddList_Function

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,38 @@
+//import { AddListName } from "./AddListName";
+import { useState } from "react";
+
 export function Card() {
-  return (
-    <>
+  const [add, setAdd] = useState(false);
+  const handleadd = () => {
+    setAdd(true);
+  };
+
+  if (add) {
+    return <AddListName />;
+  } else {
+    return (
       <article className="article-card1">
         <section className="card-box1">
-       
-          <button onClick={onClick} className="addList">+ Añade una lista</button>
+          <button onClick={handleadd} className="addList">
+            + Añade una lista
+          </button>
         </section>
       </article>
-
-    </>
-  );
+    );
+  }
 }
 
-
-function onClick() {
-    return(
-    <input className="inputTitle" type="text" placeholder="Introduzca título de la lista"></input>) 
-
+function AddListName() {
+  return (
+    <>
+      <div className="BlackCard">
+        <input
+          className="inputTitle"
+          type="text"
+          placeholder="Introduzca título de la lista"
+        ></input>
+        <button className="AddListButton">Añadir Lista</button>
+      </div>
+    </>
+  );
 }
