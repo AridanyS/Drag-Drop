916b4799d4bfbdc92cec6281679a84ffa6a6ad33
Aridany <aridany54@gmail.com>
2024-04-28 22:39:01 +0200

    Card Logic

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 //import { AddListName } from "./AddListName";
 import { useState } from "react";
-
+/*
 export function Card() {
   const [add, setAdd] = useState(false);
   const handleadd = () => {
@@ -31,12 +31,81 @@ function AddListName() {
             className="inputTitle"
             type="text"
             placeholder="Introduzca título de la lista..."
-          ></input>
+          >
+
+          </input>
           <button className="AddListButton">Añadir Lista</button>
           <button className="X">x</button>
         </div>
       </article>
     </>
   );
+}*/
+
+export function Card() {
+  const [add, setAdd] = useState(false);
+
+  const handleAdd = () => {
+    setAdd(true);
+  };
+
+  if (add) {
+    return <AddListName />;
+  } else {
+    return (
+      <article className="article-card1">
+        <section className="card-box1">
+          <button onClick={handleAdd} className="addList">
+            <span className="mas">+</span> Añade una lista
+          </button>
+        </section>
+      </article>
+    );
+  }
 }
 
+function AddListName() {
+  const [inputValue, setInputValue] = useState("");
+  const [lists, setLists] = useState([]);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleAddList();
+    }
+  };
+
+  const handleAddList = () => {
+    if (inputValue.trim() !== "") {
+      setLists([...lists, inputValue]); // Agrega el nuevo texto a la lista de textos
+      setInputValue(""); // Limpia el input
+    }
+  };
+
+  return (
+    <article className="article-card2">
+      <div className="BlackCard">
+        <input
+          className="inputTitle"
+          type="text"
+          placeholder="Introduzca título de la lista..."
+          value={inputValue}
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+        />
+        <button className="AddListButton" onClick={handleAddList}>
+          Añadir Lista
+        </button>
+        {lists.map((item, index) => (
+          <div key={index} className="list-item">
+            <p>{item}</p>
+          </div>
+        ))}
+        <button className="X">x</button>
+      </div>
+    </article>
+  );
+}
