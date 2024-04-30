ff9367ab7bddacf1a24259296a19aefd41a4c516
Aridany <aridany54@gmail.com>
2024-04-30 17:05:20 +0100

    Delete old comments

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,46 +1,5 @@
 
 import { useState } from "react";
-/*
-export function Card() {
-  const [add, setAdd] = useState(false);
-  const handleadd = () => {
-    setAdd(true);
-  };
-
-  if (add) {
-    return <AddListName />;
-  } else {
-    return (
-      <article className="article-card1">
-        <section className="card-box1">
-          <button onClick={handleadd} className="addList">
-            <span className="mas">+</span> Añade una lista
-          </button>
-        </section>
-      </article>
-    );
-  }
-}
-
-function AddListName() {
-  return (
-    <>
-      <article className="article-card2">
-        <div className="BlackCard">
-          <input
-            className="inputTitle"
-            type="text"
-            placeholder="Introduzca título de la lista..."
-          >
-
-          </input>
-          <button className="AddListButton">Añadir Lista</button>
-          <button className="X">x</button>
-        </div>
-      </article>
-    </>
-  );
-}*/
 
 export function Card() {
   const [add, setAdd] = useState(false);
@@ -80,8 +39,8 @@ function AddListName() {
 
   const handleAddList = () => {
     if (inputValue.trim() !== "") {
-      setLists([...lists, inputValue]); // Agrega el nuevo texto a la lista de textos
-      setInputValue(""); // Limpia el input
+      setLists([...lists, inputValue]);
+      setInputValue(""); 
     }
   };
 
