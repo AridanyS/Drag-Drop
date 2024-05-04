bd27a7dba772d5769abb5d1c4158ac3bf0d91318
Aridany <aridany54@gmail.com>
2024-05-04 05:03:17 +0100

    header.css, headerIcons.css, images folder, undo padding, index.html changes header

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import { Card } from './components/Cards.jsx'
-import './components/Cards.css'
-import './styles/main.css'
-import './styles/header.css'
+import { Card } from "./components/Cards.jsx";
+import "./components/Cards.css";
+import "./styles/main.css";
+import "./styles/header.css";
+import "./styles/headerIcons.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <Card />
-
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
